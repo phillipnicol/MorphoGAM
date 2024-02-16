8d3775d8c200f6e5e0fd1e36a99b64685f2b1d69
Phillip Nicol <43178367+phillipnicol@users.noreply.github.com>
2024-02-16 10:50:02 -0500

    Added ns quasipoisson

--- analysis/slideseq_ca3/code/sim_power.R
+++ analysis/slideseq_ca3/code/sim_power.R
@@ -21,16 +21,16 @@ kappa <- c(0.5, 1, 1.5, 2, 2.5, 3)
 sigma <- seq(100, 1000, by=100)
 niter <- 100
 
-res <- array(0, dim=c(length(kappa), length(sigma), 3))
+res <- array(0, dim=c(length(kappa), length(sigma),4))
 
 mu <- 1
 theta <- 5
 for(i in 1:length(kappa)) {
   for(j in 1:length(sigma)) {
-    pow1 <- 0; pow2 <- 0; pow3 <- 0
+    pow1 <- 0; pow2 <- 0; pow3 <- 0; pow4 <- 0
     for(k in 1:niter) {
-      #y <- rnegbin(n = nrow(df), mu=mu+kappa[i]*exp(-sigma[j]*(out$t - 0.5)^2), theta=theta)
-      y <- rnegbin(n = nrow(df), mu=2*sin(40*out$t)+2, theta=theta)
+      y <- rnegbin(n = nrow(df), mu=mu+kappa[i]*exp(-sigma[j]*(out$t - 0.5)^2), theta=theta)
+      #y <- rnegbin(n = nrow(df), mu=2*sin(40*out$t)+2, theta=theta)
       y <- matrix(y,nrow=1)
       y <- rbind(y,y)
       spark <- sparkx(y, locus_in=df)
@@ -47,6 +47,14 @@ for(i in 1:length(kappa)) {
                     order="Sum_coords")
       if(fit3[1,13] < 0.05/20000) {
         pow3 <- pow3 + 1
+      }
+
+      fit4 <- glm(y[1,] ~ ns(out$t, df=10), family=quasipoisson())
+      fit.null <- glm(y[1,] ~ 1, family=quasipoisson())
+      val <- anova(fit4, fit.null, test="LRT")
+      p.val <- val$`Pr(>Chi)`[2]
+      if(p.val < 0.05/20000) {
+        pow4 <- pow4+1
       }
     }
     res[i,j,1] <- pow1/niter
@@ -67,7 +75,7 @@ df <- melt(res); colnames(df) <- c("kappa", "sigma", "method", "power")
 df$kappa <- paste("k = ", kappa[df$kappa])
 df$sigma <- sigma[df$sigma]
 
-df$method <- c("SPARK-X", "Projection", "nnSVG")[df$method]
+df$method <- c("SPARK-X", "Projection", "nnSVG", "Project (ns)")[df$method]
 
 p <- ggplot(data=df,aes(x=sigma, y=power, color=method)) +
   geom_point() +
