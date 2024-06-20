465f1af662390be28119096df111a306d83ae9ed
Phillip Nicol <43178367+phillipnicol@users.noreply.github.com>
2024-06-20 15:26:59 -0400

    Updating Null test

--- analysis/slideseq_ca3/code/null_test.R
+++ analysis/slideseq_ca3/code/null_test.R
@@ -4,28 +4,56 @@ library(tidyverse)
 library(splines)
 library(mgcv)
 
-spe <- SlideSeqV2_mouseHPC()
 
+spe <- STexampleData::SlideSeqV2_mouseHPC()
 
 ixs <- which(spe$celltype == "CA3") #subset to CA3
 
 xy <- spatialCoords(spe)[ixs,]
+Y <- counts(spe)[,ixs]
 
-Y.sub <- counts(spe)[,ixs]
+xy.dist <- as.matrix(dist(xy))
+knn <- 20
+prune.outlier <- 3
+nnk <- apply(xy.dist, 1, function(x) sort(x)[knn+1])
+outlier <- which(nnk > (prune.outlier)*median(nnk))
 
-load("../data/curve_object.rda")
+xy <- xy[-outlier,]; Y <- Y[,-outlier]
 
+load("../data/ca3_curve.RData")
+t <- fit$xyt$t
 
 set.seed(1)
 #Scramble spatial locations
-Y.sub <- Y.sub[,sample(1:ncol(Y.sub), size=ncol(Y.sub), replace=FALSE)]
+Y <- Y[,sample(1:ncol(Y), size=ncol(Y), replace=FALSE)]
+Y <- Y[rowSums(Y) >= 10,]
+l.o <- log(colSums(Y))
 
+p.val <- rep(0, nrow(Y))
 #First test using GAM
-fit1 <- detectSVG(Y.sub,out)
+for(j in 1:nrow(Y)) {
+  print(j)
+  fit1 <- mgcv::gam(Y[j,] ~ s(t,bs="cr") + offset(l.o),family=nb(), H=diag(10))
+  p.val[j] <- summary(fit1)$s.pv
+}
 
-#Now test using ns
-fit2 <- detectSVGns(Y.sub,out)
+saveRDS(p.val, file= "../data/null_error_rate.RDS")
 
-df <- data.frame(gam=fit1$p.val, ns=fit2$p.val)
-saveRDS("../data/null_error_rate.RDS")
+
+## Type I error
+cutoff <- seq(10^{-4}, 0.1, length.out=1000)
+t1e <- sapply(cutoff, function(i) sum(p.val < i))/nrow(Y)
+
+p <- data.frame(x=cutoff, y=t1e) |>
+  ggplot(aes(x=x,y=y)) +
+  geom_point() +
+  geom_abline(slope=1, intercept=0, linetype="dashed", color="red") +
+  xlab("Significance level") +
+  ylab("Type I error rate") +
+  scale_x_log10() +
+  scale_y_log10() +
+  theme_bw()
+
+ggsave(filename="../plots/type_1_error.png", width=6.1, height=4.85, units="in")
+
 
