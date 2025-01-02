4ccdbacbe287550a4b0e87715559b1b435f8bd98
ggrajeda <gabriel.grajeda@gmail.com>
2025-01-02 16:51:35 -0500

    Use KNN graph to get Mapper clusters instead of DBSCAN
    Since we are already computing the KNN graph, we don't need to introduce an additional dependency on DBSCAN clustering. This also reduces the number of parameters and makes the output of the Mapper algorithm more robust to parameter changes (since the clusters are defined by the KNN graph, which contains global information).

--- R/mapper1D.R
+++ R/mapper1D.R
@@ -1,17 +1,19 @@
-get_clusters_in_level <- function(data, is_in_level, eps, min_pts) {
+# Returns the components in a graph's level set.
+get_clusters_in_level <- function(graph, is_in_level) {
   if (!any(is_in_level)) { return(list()) }
 
-  level_data <- data[is_in_level, ]
-  level_dbscan <- dbscan::dbscan(level_data, eps = eps, minPts = min_pts)
-  cluster_indices <- level_dbscan$cluster
-  num_clusters <- max(cluster_indices)
+  level_vertices <- igraph::V(graph)[is_in_level]
+  level_subgraph <- igraph::induced_subgraph(graph, level_vertices)
+  level_components <- igraph::components(level_subgraph)
 
-  clusters <- lapply(seq_len(num_clusters), function(cluster_index) {
-    which(is_in_level)[cluster_indices == cluster_index]
+  clusters <- lapply(seq_along(level_components$csize), function(component) {
+    is_in_component <- (level_components$membership == component)
+    names(igraph::V(level_subgraph)[is_in_component])
   })
   return(clusters)
 }
 
+# Returns the pairs of vertices from different level sets that share a point.
 get_mapper_edges <- function(vertices, level1, level2) {
   clusters_overlap <- function(v1, v2) {
     cluster1 <- unlist(vertices[v1]$points)
@@ -33,54 +35,55 @@ get_mapper_edges <- function(vertices, level1, level2) {
 #' for the analysis of high dimensional data sets and 3d object recognition."
 #' SPBG. 2007.
 #'
-#' @param data A numeric matrix (n x 2) representing spatial coordinates.
-#' @param filter_values A numeric vector (n) of the filter values on the data.
+#' @param graph An igraph object (n vertices).
+#' @param filter_values A numeric vector (n) of filter values, specifying how to
+#'   order the vertices of the graph and thus group them into level sets.
 #' @param num_intervals An integer specifying the resolution of the Mapper
-#'   algorithm (how many level sets to cluster)
+#'   algorithm (the number of level sets).
 #' @param percent_overlap A numeric between 0 and 100 specifying the gain of
-#'   Mapper algorithm (what percent should level sets overlap)
-#' @param eps A numeric specifying the epsilon radius that defines neighboring
-#'   points in DBSCAN.
-#' @param min_pts An integer specifying the minimum cluster size for DBSCAN.
+#'   Mapper algorithm (the percentage that level sets should overlap).
 #'
 #' @return igraph object containing the Mapper graph
 #'
 #' @examples
 #' my_data <- makeSwissRoll()
-#' scaled_data <- scale(my_data)
-#' pc <- prcomp(scaled_data, center = FALSE, scale = FALSE)
-#' mapper_graph <- get_mapper_graph(scaled_data, pc$x[, 1], num_intervals = 7,
-#'                                  eps = 0.2, min_pts = 5)
-#' get_mapper_plot(scaled_data, mapper_graph)
+#' my_graph <- dimRed:::makeKNNgraph(x = my_data, k = 5, eps = 0)
+#' mapper_graph <- get_mapper_graph(my_graph, my_data[, 1])
+#' get_mapper_plot(my_data, mapper_graph)
 get_mapper_graph <- function(
-    data,
+    graph,
     filter_values,
     num_intervals = 10,
-    percent_overlap = 30,
-    eps = 0.1,
-    min_pts = 50
+    percent_overlap = 33
 ) {
   if (percent_overlap >= 50) {
     warning("Overlap should be under 50%.")
+  }
+  # Vertex names are necessary to identify subgraph components.
+  vertex_names <- names(igraph::V(graph))
+  if (is.null(vertex_names)) {
+    igraph::V(graph)$name <- as.character(igraph::V(graph))
   }
 
   filter_min <- min(filter_values)
   filter_max <- max(filter_values)
   overlap <- percent_overlap / 100
-  interval_span <- (num_intervals - (num_intervals - 1) * overlap)
-  interval_length <- (filter_max - filter_min) / interval_span
+  interval_scaling_factor <- (num_intervals - (num_intervals - 1) * overlap)
+  interval_length <- (filter_max - filter_min) / interval_scaling_factor
   step_size <- interval_length * (1 - overlap)
 
-  # Iterate through each level set and cluster the data.
+  # Iterate through each level set and identify the components of its subgraph.
   mapper_graph <- igraph::make_empty_graph(directed = FALSE)
   for (level in 1:num_intervals) {
     lo <- filter_min + (level - 1) * step_size
     hi <- lo + interval_length
     is_in_level <- (lo <= filter_values) & (filter_values <= hi)
-    level_clusters <- get_clusters_in_level(data, is_in_level, eps, min_pts)
+    level_clusters <- get_clusters_in_level(graph, is_in_level)
 
-    # Add each cluster as a vertex in the graph.
+    # Add each component as a vertex in the graph.
     for (cluster in level_clusters) {
+      # TODO: Consider ignoring small clusters.
+      # if (length(cluster) < 5) next
       attributes <- list(level=level, points=list(cluster))
       mapper_graph <- igraph::add_vertices(mapper_graph, 1, attr=attributes)
     }
@@ -92,13 +95,58 @@ get_mapper_graph <- function(
   return(mapper_graph)
 }
 
-get_mapper_plot <- function(data, mapper_graph) {
-  points <- igraph::V(mapper_graph)$points
-  x_coords <- lapply(points, function(x) mean(data[x, 1]))
-  y_coords <- lapply(points, function(x) mean(data[x, 2]))
+# Returns the length of the largest cycle in the graph.
+#
+# This is implemented inefficiently, by finding all simple paths between
+# neighbors in the graph, but it should be fast enough for small graphs.
+get_largest_cycle_length <- function(graph) {
+  paths_to_neighbors <- lapply(igraph::V(graph), function(v) {
+    curr_level <- igraph::V(graph)[v]$level
+    # Performance optimization: Only consider neighbors from the previous level.
+    prev_neighbors <- igraph::neighbors(graph, v)[level == curr_level - 1]
+    if (!any(prev_neighbors)) { return(NULL) }
+    igraph::all_simple_paths(graph, v, prev_neighbors)
+  })
+  paths_to_neighbors <- unlist(paths_to_neighbors, recursive = FALSE)
+  path_lengths <- vapply(paths_to_neighbors, length, FUN.VALUE=numeric(1))
+  # Ignore trivial paths between neighbors.
+  if (all(path_lengths <= 2)) { return(0) }
+  max(path_lengths[path_lengths > 2])
+}
+
+#' Determines if a k-nearest neighbors graph of a dataset forms a loop.
+#'
+#' Loops are detected as large cycles in the output of the Mapper algorithm.
+#'
+#' @param xy A numeric matrix or data frame with two columns representing the x
+#'   and y coordinates of the data points.
+#' @param knn_graph A k-nearest neighbors graph of `xy`.
+#'
+#' @return Whether the k-nearest neighbors graph is a loop.
+#'
+#' @examples
+#' my_data <- makeSwissRoll()
+#' my_graph <- dimRed:::makeKNNgraph(x = my_data, k = 5, eps = 0)
+#' testthat::expect_false(is_loop(my_data, my_graph))
+is_loop <- function(xy, knn_graph) {
+  principal_components <- stats::prcomp(xy, center = TRUE, scale = TRUE)
+  num_intervals <- 10
+  mapper_graph <- get_mapper_graph(knn_graph, principal_components$x[, 1],
+                                   num_intervals = num_intervals,
+                                   percent_overlap = 33)
+  cycle_length <- get_largest_cycle_length(mapper_graph)
+  # True when a cycle takes up ~1/2 the range of the first principal component.
+  return(cycle_length >= num_intervals)
+}
+
+# Temporary helper function to visualize the output of the Mapper algorithm.
+get_mapper_plot <- function(xy, mapper_graph) {
+  points <- lapply(igraph::V(mapper_graph)$points, as.numeric)
+  x_coords <- lapply(points, function(p) mean(xy[p, 1]))
+  y_coords <- lapply(points, function(p) mean(xy[p, 2]))
   coordinates <- cbind(unlist(x_coords), unlist(y_coords))
-  plot(data[, 1], data[, 2])
+  plot(xy)
   plot(mapper_graph, layout=coordinates, add = TRUE, rescale = FALSE,
-       vertex.label = NA, vertex.size = 15, vertex.color = "orange",
+       vertex.label = NA, vertex.size = 10, vertex.color = "orange",
        edge.color = "red")
 }
