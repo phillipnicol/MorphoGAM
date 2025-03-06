7ca05459bd9ea7ccf820823f2118145858e40e7b
Phillip Nicol <43178367+phillipnicol@users.noreply.github.com>
2025-03-06 06:40:15 -0500

    Auto-commit from makefile

---
+++ analysis/merfish_swissroll/code/draw_fibroblasts.R
@@ -0,0 +1,62 @@
+
+setwd(here::here("analysis/merfish_swissroll/code"))
+
+library(anndata)
+
+ad <- anndata::read_h5ad("../../data/adata_swiss_2023_10_31_annotated_clean.h5ad")
+
+
+meta <- ad$obs
+
+library(MorphoGAM)
+library(tidyverse)
+
+meta.sub1 <- meta |> filter(Mouse_ID == "061923_D9_m2_Swiss") |>
+  filter(Tier1 %in% c("Fibroblast", "Epithelial"))
+
+xy <- meta.sub1 |> select(x,y) |> as.matrix()
+fit <- CurveFinderInteractive(xy)
+
+save(fit, file="../data/061923_D9_m2_Swiss_fibroblast.RData")
+
+# Save matrix
+
+Y <- ad$raw[rownames(meta.sub1),]
+Y <- as(t(Y), "sparseMatrix")
+Matrix::writeMM(Y, file="../data/061923_D9_m2_Swiss_fibroblast_counts.mtx")
+
+
+meta.sub2 <- meta |> filter(Mouse_ID == "080823_D9_m5_Swiss") |>
+  filter(Tier1 %in% c("Fibroblast", "Epithelial"))
+
+
+xy <- meta.sub2 |> select(x,y) |> as.matrix()
+fit <- CurveFinderInteractive(xy)
+
+save(fit, file="../data/080823_D9_m5_Swiss_fibroblast.RData")
+
+# Save matrix
+
+Y <- ad$raw[rownames(meta.sub2),]
+Y <- as(t(Y), "sparseMatrix")
+Matrix::writeMM(Y, file="../data/080823_D9_m5_Swiss_fibroblast_counts.mtx")
+
+
+
+meta.sub3 <- meta |> filter(Mouse_ID == "080823_D9_m13_Swiss") |>
+  filter(Tier1 %in% c("Fibroblast", "Epithelial"))
+
+
+
+xy <- meta.sub3 |> select(x,y) |> as.matrix()
+fit <- CurveFinderInteractive(xy)
+
+save(fit, file="../data/080823_D9_m13_Swiss_fibroblast.RData")
+
+# Save matrix
+
+Y <- ad$raw[rownames(meta.sub3),]
+Y <- as(t(Y), "sparseMatrix")
+Matrix::writeMM(Y, file="../data/080823_D9_m5_Swiss_fibroblast_counts.mtx")
+
+
---
+++ analysis/merfish_swissroll/code/run_baysor_morphogam_fibroblast.R
@@ -0,0 +1,48 @@
+
+
+### Curve1
+
+load("../data/061923_D9_m2_Swiss_fibroblast.RData")
+
+Y <- Matrix::readMM("../data/061923_D9_m2_Swiss_fibroblast_counts.mtx")
+
+Y <- as.matrix(Y)
+
+
+mgam <- MorphoGAM(Y=Y,curve.fit=fit,
+                  design=y~s(t) + s(r))
+
+save(mgam, file="../data/mgam_d9_061923_fibroblast_baysor.RData")
+
+
+### Curve 2
+
+
+load("../data/curve_d9_m5_080823.RData")
+
+Y <- Matrix::readMM("../data/080823_d9_m5.mtx")
+
+Y <- as.matrix(Y)
+
+
+mgam <- MorphoGAM(Y=Y,curve.fit=fit,
+                  design=y~s(t) + s(r))
+
+save(mgam, file="../data/mgam_d9_m5_080823_baysor.RData")
+
+
+
+### Curve 3
+
+load("../data/curve_d9_m13_080823.RData")
+
+Y <- Matrix::readMM("../data/080823_d9_m13.mtx")
+
+Y <- as.matrix(Y)
+
+
+mgam <- MorphoGAM(Y=Y,curve.fit=fit,
+                  design=y~s(t) + s(r))
+
+save(mgam, file="../data/mgam_d9_m13_080823_baysor.RData")
+
