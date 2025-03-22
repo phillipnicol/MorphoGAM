b910031d2b87c78dca54f3324a71062fd065d019
Phillip Nicol <43178367+phillipnicol@users.noreply.github.com>
2025-03-22 09:54:29 -0400

    Auto-commit from makefile

--- analysis/merfish_swissroll/code/analyze_fibroblasts.R
+++ analysis/merfish_swissroll/code/analyze_fibroblasts.R
@@ -20,10 +20,92 @@ rownames(mgam$fxs.t) <- gene_names
 rownames(mgam$fpca.t$u) <- gene_names
 rownames(mgam$fpca.t$u) <- gene_names
 
-t.range <- c(0.95, 1)
-ixs <- which(fit$xyt$t > t.range[1] & fit$xyt$t < t.range[2])
+# Find genes correlated with Vil1
+vil1.1 <- mgam$fxs.t["Vil1",]
 
-my.mean <- rowMeans(mgam$fxs.t[,ixs])
+cos.sim1 <- apply(mgam$fxs.t, 1, function(x) {
+  sum(x*vil1)/(sqrt(sum(x^2)*sum(vil1^2)))
+})
+
+mgam1 <- mgam
+
+
+## Two
+
+
+load("../data/mgam_d9_m5_080823_fibroblast_baysor.RData")
+
+load("../data/080823_D9_m5_Swiss_fibroblast.RData")
+
+Y <- Matrix::readMM("../data/080823_D9_m5_Swiss_fibroblast_counts.mtx")
+
+
+rownames(Y) <- gene_names
+Y2 <- Y
+rownames(mgam$results) <- gene_names
+rownames(mgam$fxs.r) <- gene_names
+rownames(mgam$fxs.t) <- gene_names
+rownames(mgam$fpca.t$u) <- gene_names
+rownames(mgam$fpca.t$u) <- gene_names
+
+# Find genes correlated with Vil1
+vil1.2 <- mgam$fxs.t["Vil1",]
+
+cos.sim2 <- apply(mgam$fxs.t, 1, function(x) {
+  sum(x*vil1.2)/(sqrt(sum(x^2)*sum(vil1.2^2)))
+})
+
+cos.sim2[order(abs(cos.sim2), decreasing = TRUE)[1:10]]
+
+mgam2 <- mgam
+
+
+## Third
+
+
+
+load("../data/mgam_d9_m13_080823_fibroblast_baysor.RData")
+
+load("../data/080823_D9_m13_Swiss_fibroblast.RData")
+
+Y <- Matrix::readMM("../data/080823_D9_m13_Swiss_fibroblast_counts.mtx")
+
+
+
+
+rownames(Y) <- gene_names
+Y3 <- Y
+rownames(mgam$results) <- gene_names
+rownames(mgam$fxs.r) <- gene_names
+rownames(mgam$fxs.t) <- gene_names
+rownames(mgam$fpca.t$u) <- gene_names
+rownames(mgam$fpca.t$u) <- gene_names
+
+# Find genes correlated with Vil1
+vil1.3 <- mgam$fxs.t["Vil1",]
+
+cos.sim3 <- apply(mgam$fxs.t, 1, function(x) {
+  sum(x*vil1.3)/(sqrt(sum(x^2)*sum(vil1.3^2)))
+})
+
+cos.sim3[order(abs(cos.sim3), decreasing = TRUE)[1:10]]
+
+mgam3 <- mgam
+
+
+df <- data.frame(roll1 = cos.sim1,
+                 roll2 = cos.sim2,
+                 roll3 = cos.sim3)
+
+
+df$neg.markers <- pmin(df$roll1, df$roll2, df$roll3)
+df$pos.markers <- pmax(df$roll1, df$roll2, df$roll3)
+
+df |> arrange(desc(neg.markers)) |> head(n=10)
+
+df |> arrange(pos.markers) |> head(n=10)
+
+
 
 
 
