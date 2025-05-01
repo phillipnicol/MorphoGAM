00e8924949fb24c21798c19419b2c0cb5d11352f
Phillip Nicol <43178367+phillipnicol@users.noreply.github.com>
2025-05-01 11:13:03 -0400

    Auto-commit from makefile

--- analysis/merfish_swissroll/code/intercept_model.R
+++ analysis/merfish_swissroll/code/intercept_model.R
@@ -84,13 +84,13 @@ res3 <- readRDS("../data/roll3_intercept_model.RDS")
 
 
 library(tidyverse)
-df <- data.frame(gene = rownames(res1),
+beta.combine <- data.frame(gene = rownames(res1),
                  beta1 = res1[,1],
                  beta2 = res2[,1],
                  beta3 = res3[,1],
-                 pval = pmax(res1[,3], res2[,3], res3[,3]))
+                 pval = 1 - (1-pmax(res1[,3], res2[,3], res3[,3])^3))
 
-df$pval <- p.adjust(df$pval, method = "BH")
+beta.combine$pval <- p.adjust(beta.combine$pval, method = "BH")
 
 #df <- df |> mutate(stat = -2*stat) |>
 #  mutate(pval = pchisq(stat, df = 6, lower.tail=FALSE)) |>
@@ -103,7 +103,7 @@ df$pval <- p.adjust(df$pval, method = "BH")
 
 #df |> arrange(pval) |> head(n=20)
 
-up.genes <- df |> mutate(max.min = beta1 + beta2 + beta3) |>
+up.genes <- beta.combine |> mutate(max.min = pmin(beta1, beta2, beta3)) |>
   arrange(desc(max.min))
 
 
@@ -259,25 +259,31 @@ ggsave(p.pos, filename="../plots/ulcerated_intercept_pos.png",
 
 ### Counts
 
+gene.names <- c("Mmp3", "Selp", "Mmp10", "Col12a1", "Il11")
 
 
 
-
-
-
+Y1.dn <- sweep(as.matrix(Y1), MARGIN = 2, FUN = "/", STATS = colSums(Y1))
+fxs.t <- Y1
 indic <- rep(0, length(fit1$xyt$t))
 
 indic[fit1$xyt$t > 0.95 & fit1$xyt$t < 0.97] <- 1
 
-fxs.t[,indic == 0] <- 0
-fxs.t[,indic == 1] <- res1[,1]
+fxs.t[,indic == 0] <- exp(res1[,2])
+fxs.t[,indic == 1] <- exp(res1[,2] + res1[,1])
 
-
-
-df <- t(Y1[c("Mmp3", "Plek", "Nos3"),]) |>
+df <- t(Y1.dn[gene.names,]) |>
   as.data.frame() |>
   mutate(t = fit1$xyt$t) |>
   pivot_longer(-t)
+
+
+df.fit <- t(fxs.t[gene.names,]) |>
+  as.data.frame() |>
+  mutate(t = fit1$xyt$t) |>
+  pivot_longer(-t)
+
+df$fitted <- df.fit$value
 
 # Arrange and identify horizontal segments
 segments <- df %>%
@@ -294,32 +300,44 @@ segments <- df %>%
 # Plot horizontal segments only
 p1 <- ggplot(df, aes(x = t, y = value)) +
   theme_bw() +
-  geom_point() +
-  annotate("rect", xmin = 0.95, xmax = 0.97, ymin = -Inf, ymax = Inf,
+  geom_point(size=0.25) +
+  annotate("rect", xmin = 0.95, xmax = 0.97, ymin = 0, ymax = Inf,
            fill = "lightgrey", alpha = 0.5) +
   ylab("Log FC from baseline") +
-  facet_wrap(~name) +
+  facet_wrap(~name, ncol= 1) +
+  geom_line(aes(x = t, y = fitted, color = name)) +
   labs(color = "Gene") +
-  xlim(c(0.9, 1))
+  scale_y_sqrt()
 
 
 
 ### Roll 2
 
 
-
-
+Y2 <- as.matrix(Y2)
+Y2.dn <- sweep(as.matrix(Y2), MARGIN = 2, FUN = "/", STATS = colSums(Y2))
+fxs.t <- Y2 |> as.matrix()
 indic <- rep(0, length(fit2$xyt$t))
+
 
 indic[fit2$xyt$t > 0.25 & fit2$xyt$t < 0.6] <- 1
 indic[fit2$xyt$t > 0.78 & fit2$xyt$t < 0.8] <- 1
 indic[fit2$xyt$t > 0.88 & fit2$xyt$t < 0.9] <- 1
 
+fxs.t[,indic == 0] <- exp(res2[,2])
+fxs.t[,indic == 1] <- exp(res2[,2] + res1[,1])
 
-df <- t(as.matrix(Y2)[c("Mmp3", "Plek", "Nos3"),]) |>
+df <- t(Y2.dn[gene.names,]) |>
   as.data.frame() |>
   mutate(t = fit2$xyt$t) |>
   pivot_longer(-t)
+
+df.fit <- t(fxs.t[gene.names,]) |>
+  as.data.frame() |>
+  mutate(t = fit2$xyt$t) |>
+  pivot_longer(-t)
+
+df$fitted <- df.fit$value
 
 # Arrange and identify horizontal segments
 segments <- df %>%
@@ -336,7 +354,7 @@ segments <- df %>%
 # Plot horizontal segments only
 p2 <- ggplot(df, aes(x = t, y = value)) +
   theme_bw() +
-  geom_point() +
+  geom_point(size=0.25) +
   annotate("rect", xmin = 0.25, xmax = 0.6, ymin = -Inf, ymax = Inf,
            fill = "lightgrey", alpha = 0.5) +
   annotate("rect", xmin = 0.78, xmax = 0.8, ymin = -Inf, ymax = Inf,
@@ -344,7 +362,48 @@ p2 <- ggplot(df, aes(x = t, y = value)) +
   annotate("rect", xmin = 0.88, xmax = 0.9, ymin = -Inf, ymax = Inf,
            fill = "lightgrey", alpha = 0.5) +
   ylab("Log FC from baseline") +
-  facet_wrap(~name) +
+  geom_line(aes(x = t, y = fitted, color = name)) +
+  facet_wrap(~name, ncol = 1) +
+  labs(color = "Gene")
+
+
+
+### Roll 3
+
+indic <- rep(0, length(fit3$xyt$t))
+
+indic[fit3$xyt$t > 0.4 & fit3$xyt$t < 0.7] <- 1
+
+
+Y3 <- as.matrix(Y3)
+Y3.dn <- sweep(as.matrix(Y3), MARGIN = 2, FUN = "/", STATS = colSums(Y3))
+fxs.t <- Y3 |> as.matrix()
+
+fxs.t[,indic == 0] <- exp(res3[,2])
+fxs.t[,indic == 1] <- exp(res3[,2] + res3[,1])
+
+df <- t(Y3.dn[gene.names,]) |>
+  as.data.frame() |>
+  mutate(t = fit3$xyt$t) |>
+  pivot_longer(-t)
+
+df.fit <- t(fxs.t[gene.names,]) |>
+  as.data.frame() |>
+  mutate(t = fit3$xyt$t) |>
+  pivot_longer(-t)
+
+
+df$fitted <- df.fit$value
+
+# Plot horizontal segments only
+p3 <- ggplot(df, aes(x = t, y = value)) +
+  geom_point(size=0.25) +
+  theme_bw() +
+  annotate("rect", xmin = 0.4, xmax = 0.7, ymin = -Inf, ymax = Inf,
+           fill = "lightgrey", alpha = 0.5) +
+  ylab("Log FC from baseline") +
+  geom_line(aes(x = t, y = fitted, color = name)) +
+  facet_wrap(~name, ncol = 1) +
   labs(color = "Gene")
 
 
